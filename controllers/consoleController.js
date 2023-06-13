424117838fceb9c1321fc6e3f1a76bfc4dcf7288
razmanarif <razmanarif@gmail.com>
2023-06-13 17:12:15 +0800

    added edit and search function

--- controllers/consoleController.js
+++ controllers/consoleController.js
@@ -12,15 +12,54 @@ router.get("/", async(req,res) => {
 })
 
 // showing all games for a console
-router.get('/console/:id/details', async(req,res) => {
-    let searchOptions = {}
-    try{
-        const consoleItem = await ConsoleModel.findById(req.params.id)
-        res.render('console/showGames', {consoleItem: consoleItem})
-    } catch (e) {
-        console.log (e)
-    }
-})
+// router.get('/console/:id/details', async(req,res) => {
+//     try{
+//         const consoleItem = await ConsoleModel.findById(req.params.id)
+//         res.render('console/showGames', {consoleItem: consoleItem})
+//     } catch (e) {
+//         console.log (e)
+//     }
+
+// })
+router.get('/console/:id/details', async (req, res) => {
+  try {
+      const consoleItem = await ConsoleModel.findById(req.params.id);
+
+      const searchQuery = req.query.gameName;
+      let games = consoleItem.games;
+
+      if (searchQuery && searchQuery.trim() !== '') {
+          // Filter games based on the search query
+          games = games.filter(game => game.gameName.toLowerCase().includes(searchQuery.toLowerCase()));
+      }
+
+      res.render('console/showGames', { consoleItem: consoleItem, games: games, searchQuery: searchQuery });
+  } catch (e) {
+      console.log(e);
+  }
+});
+
+
+
+
+// router.get('/console/:id/details', async (req, res) => {
+//   try {
+//       const consoleItem = await ConsoleModel.findById(req.params.id);
+
+//       let games = consoleItem.games;
+//       const searchQuery = req.query.gameName;
+
+//       if (searchQuery && searchQuery.trim() !== '') {
+//           // Filter games based on the search query
+//           games = games.filter(game => game.gameName.includes(searchQuery));
+//       }
+
+//       res.render('console/showGames', { consoleItem: consoleItem, games: games });
+//   } catch (e) {
+//       console.log(e);
+//   }
+// });
+
 
 // Adding consoles to main page
 router.get("/add_console", (req,res) =>{
@@ -69,6 +108,7 @@ router.post('/games/:id/add', async (req, res) => {
 // Editing games inside console
 router.get("/console/:consoleid/games/:id/update", async (req,res) =>{
     const games = await ConsoleModel.findOne({'games._id': req.params.id}, {"games.$": 1})
+    consoleId = req.params.consoleid
 
     const game = games.games[0]
     // console.log(DateTime.fromJSDate(game.dateBoughtGame).toFormat("yyyy-MM-dd"))
@@ -78,15 +118,49 @@ router.get("/console/:consoleid/games/:id/update", async (req,res) =>{
       gameName: game.gameName,
       gamePrice: game.gamePrice,
       isCompleted: game.isCompleted,
-      hoursPlayed: game.hoursPlayed
-    }
+      hoursPlayed: game.hoursPlayed,
+      
+    }, game, consoleId
   })
-    // console.log(req.params.consoleid)
-    // console.log(req.params.id)
-    // console.log(games)
 })
 
-//router.put to edit games go here
+
+router.put("/console/:consoleid/games/:id/update", async (req, res) => {
+  try {
+    const consoleId = req.params.consoleid;
+    const gameId = req.params.id;
+    const updatedGame = req.body;
+
+    // find console by id
+    const console = await ConsoleModel.findById(consoleId);
+
+    if (!console) {
+      return res.send("Console not found");
+    }
+
+    // Find the game within the console's games array by its ID
+    const game = console.games.id(gameId);
+
+    if (!game) {
+      return res.send("Game not found");
+    }
+
+    // updating game details
+    game.gameName = updatedGame.gameName;
+    game.dateBoughtGame = updatedGame.dateBoughtGame;
+    game.gamePrice = updatedGame.gamePrice;
+    game.isCompleted = updatedGame.isCompleted;
+    game.hoursPlayed = updatedGame.hoursPlayed;
+
+
+    await console.save();
+    res.redirect('/');
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 
 
@@ -101,6 +175,7 @@ router.delete("/console/:consoleid/games/:id", async (req, res) => {
       // Remove the game from the games array
       console.games.pull(req.params.id);
       await console.save();
+      res.redirect("/")
   
     } catch (e) {
       console.error(e);
@@ -110,3 +185,18 @@ router.delete("/console/:consoleid/games/:id", async (req, res) => {
   
 
 module.exports = router
+
+// router.put("/console/:consoleid/games/:id/update", async (req, res) => {
+//   const values = req.body
+//   ConsoleModel.findByIdAndUpdate(req.params.id, {
+//     game.gameName: values.gameName,
+//     dateBoughtGame: values.dateBoughtGame,
+//     gamePrice: values.gamePrice,
+//     isCompleted: values.isCompleted,
+//     hoursPlayed: values.hoursPlayed
+//   }).then(()=>{
+//     console.log("update successful")
+//     res.redirect("/")
+//   }).catch(e =>console.log(e))
+
+// });
